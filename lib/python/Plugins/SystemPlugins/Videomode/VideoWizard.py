5ac3cee3c96122c7bc3d90d743a3ac74b52008d3
ACJTeam <magicobuendia@hotmail.com>
2019-03-09 11:52:48 +0100

    Add files via upload

--- lib/python/Plugins/SystemPlugins/Videomode/VideoWizard.py
+++ lib/python/Plugins/SystemPlugins/Videomode/VideoWizard.py
@@ -1,4 +1,3 @@
-from boxbranding import getBoxType
 from Screens.Wizard import WizardSummary
 from Screens.WizardLanguage import WizardLanguage
 from Screens.Rc import Rc
@@ -12,17 +11,6 @@ from Tools.Directories import resolveFilename, SCOPE_PLUGINS
 from Tools.HardwareInfo import HardwareInfo
 
 config.misc.showtestcard = ConfigBoolean(default = False)
-
-try:
-	file = open("/proc/stb/info/chipset", "r")
-	chipset = file.readline().strip()
-	file.close()
-except:
-	chipset = "unknown"
-
-has_rca = False
-if getBoxType() in ('mutant51', 'ax51', 'gb800seplus', 'gb800ueplus', 'gbquad', 'gbquadplus', 'gbipbox', 'gbultra', 'gbultraue', 'gbultraueh', 'gbultrase', 'vuduo2', 'vusolo2', 'vuzero', 'vusolo', 'vuultimo', 'spycat', 'quadbox2400', 'gbx1', 'gbx2', 'gbx3', 'gbx3h'):
-	has_rca = True
 
 class VideoWizardSummary(WizardSummary):
 	def __init__(self, session, parent):
@@ -72,6 +60,7 @@ class VideoWizard(WizardLanguage, Rc):
 		self.mode = None
 		self.rate = None
 
+
 	def createSummary(self):
 		print "[VideoWizard] createSummary"
 		from Screens.Wizard import WizardSummary
@@ -93,8 +82,6 @@ class VideoWizard(WizardLanguage, Rc):
 				descr = port
 				if descr == 'DVI' and has_hdmi:
 					descr = 'HDMI'
-				if descr == 'Scart' and has_rca:
-					descr = 'RCA'
 				if port != "DVI-PC":
 					list.append((descr,port))
 		list.sort(key = lambda x: x[0])
@@ -115,8 +102,6 @@ class VideoWizard(WizardLanguage, Rc):
 			picname = self.selection
 			if picname == 'DVI' and has_hdmi:
 				picname = "HDMI"
-			if picname == 'Scart' and has_rca:
-				picname = "RCA"					
 			self["portpic"].instance.setPixmapFromFile(resolveFilename(SCOPE_PLUGINS, "SystemPlugins/Videomode/" + picname + ".png"))
 
 	def inputSelect(self, port):
@@ -168,8 +153,6 @@ class VideoWizard(WizardLanguage, Rc):
 			print "[VideoWizard] mode:", mode
 			if mode[0] == querymode:
 				for rate in mode[1]:
-					if rate in ("auto") and not SystemInfo["Has24hz"]:
-						continue
 					if self.port == "DVI-PC":
 						print "[VideoWizard] rate:", rate
 						if rate == "640x480":
