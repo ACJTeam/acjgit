330d96d77012b4479ce41c0021d09fd38eb9126f
ACJTeam <magicobuendia@hotmail.com>
2019-04-19 20:15:33 +0200

    Add files via upload

--- lib/python/Tools/HardwareInfo.py
+++ lib/python/Tools/HardwareInfo.py
@@ -1,85 +1,75 @@
-from boxbranding import *
-
-hw_info = None
+from boxbranding import getBoxType, getBrandOEM, getMachineName
+from Components.About import about
 
 class HardwareInfo:
-	device_name = _("unavailable")
-	device_model = None
-	device_version = ""
-	device_revision = ""
-	device_hdmi = False
+	device_name = None
+	device_version = None
 
 	def __init__(self):
-		global hw_info
-		if hw_info:
+		if HardwareInfo.device_name is not None:
+#			print "using cached result"
 			return
-		hw_info = self
 
-		print "[HardwareInfo] Scanning hardware info"
-		# Version
+		HardwareInfo.device_name = "unknown"
 		try:
-			self.device_version = open("/proc/stb/info/version").read().strip()
+			file = open("/proc/stb/info/model", "r")
+			HardwareInfo.device_name = file.readline().strip()
+			file.close()
+			if getBrandOEM() == "dags":
+				HardwareInfo.device_name = "dm800se"
+			try:
+				file = open("/proc/stb/info/version", "r")
+				HardwareInfo.device_version = file.readline().strip()
+				file.close()
+			except:
+				pass
 		except:
-			pass
-
-		# Revision
-		try:
-			self.device_revision = open("/proc/stb/info/board_revision").read().strip()
-		except:
-			pass
-
-		# Name ... bit odd, but history prevails
-		try:
-			self.device_name = open("/proc/stb/info/model").read().strip()
-		except:
-			pass
-
-		# Model
-		for line in open((resolveFilename(SCOPE_SKIN, 'hw_info/hw_info.cfg')), 'r'):
-			if not line.startswith('#') and not line.isspace():
-				l = line.strip().replace('\t', ' ')
-				if ' ' in l:
-					infoFname, prefix = l.split()
-				else:
-					infoFname = l
-					prefix = ""
-				try:
-					self.device_model = prefix + open("/proc/stb/info/" + infoFname).read().strip()
-					break
-				except:
-					pass
-
-		self.device_model = self.device_model or self.device_name
-
-		# map for Xtrend device models to machine names
-		self.machine_name = "%s%s%s" % (self.device_model[:3], "x", self.device_model[-2:]) if self.device_model.startswith(("et9", "et4", "et5", "et6")) else self.device_model
-
-		# only some early DMM boxes do not have HDMI hardware
-		self.device_hdmi =  self.device_model not in ("dm7025", "dm800", "dm8000")
-
-		print "Detected: " + self.get_device_string()
+			print "----------------"
+			print "you should upgrade to new drivers for the hardware detection to work properly"
+			print "----------------"
+			print "fallback to detect hardware via /proc/cpuinfo!!"
+			try:
+				rd = open("/proc/cpuinfo", "r").read()
+				if "Brcm4380 V4.2" in rd:
+					HardwareInfo.device_name = "dm8000"
+					print "dm8000 detected!"
+				elif "Brcm7401 V0.0" in rd:
+					HardwareInfo.device_name = "dm800"
+					print "dm800 detected!"
+				elif "MIPS 4KEc V4.8" in rd:
+					HardwareInfo.device_name = "dm7025"
+					print "dm7025 detected!"
+			except:
+				pass
 
 	def get_device_name(self):
-		return hw_info.device_name
-
-	def get_device_model(self):
-		return hw_info.device_model
+		return HardwareInfo.device_name
 
 	def get_device_version(self):
-		return hw_info.device_version
+		return HardwareInfo.device_version
 
-	def get_device_revision(self):
-		return hw_info.device_revision
+	def get_device_model(self):
+		return getBoxType()
 
-	def get_device_string(self):
-		if hw_info.device_revision:
-			return "%s (%s-%s)" % (hw_info.device_model, hw_info.device_revision, hw_info.device_version)
-		elif hw_info.device_version:
-			return "%s (%s)" % (hw_info.device_model, hw_info.device_version)
-		return hw_info.device_model
+	def get_vu_device_name(self):
+		return getBoxType()
 
-	def get_machine_name(self):
-		return hw_info.machine_name
+	def get_friendly_name(self):
+		return getMachineName()
 
 	def has_hdmi(self):
-		return hw_info.device_hdmi
+		return not (HardwareInfo.device_name == 'dm800' or (HardwareInfo.device_name == 'dm8000' and HardwareInfo.device_version == None))
+
+	def linux_kernel(self):
+		try:
+			return open("/proc/version","r").read().split(' ', 4)[2].split('-',2)[0]
+		except:
+			return "unknown"
+
+	def has_deepstandby(self):
+		return getBoxType() != 'dm800'
+
+	def is_nextgen(self):
+		if about.getCPUString() in ('BCM7346B2', 'BCM7425B2', 'BCM7429B0'):
+			return True
+		return False
